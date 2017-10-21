8b7eadc51eee2a90a38e7b0c3719529c829e68d4
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-10-21 19:19:34 +0100

    Improve marker popups, start to add marker clustering

--- tweets/static/tweets/js/main.js
+++ tweets/static/tweets/js/main.js
@@ -1,6 +1,6 @@
 $(function() {
 	// Initialise the map and set default values
-	var mapleMap = L.map('mapleMap').setView([51.379597, -2.358244], 14);
+	var mapleMap = L.map('mapleMap').setView([51.379597, -2.358244], 16);
 
 	L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/{id}/{z}/{x}/{y}?access_token={accessToken}', {
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -26,10 +26,21 @@ $(function() {
           markers.push(marker);
 
 
-          marker.marker.bindPopup(marker.data.name + '<br>' + marker.data.wheelchair).addTo(mapleMap);
+          marker.marker.bindPopup(
+            'Name: ' + marker.data.name + '<br>' +
+            'Accessible: ' + marker.data.wheelchair + '<br>' +
+            'Category: ' + marker.data.type
+          ).addTo(mapleMap);
 				  var wheelchairFriendly = location['properties']['wheelchair'];
+
+				  var clusterGroup = L.markerClusterGroup();
+				  // for (i = 0; i < markers.length; i++) {
+          //   clusterGroup.addLayer(L.marker([51, -2]));
+          // }
+          console.log(markers[0]);
+          clusterGroup.addLayer(L.marker([51, -2]));
+				  mapleMap.addLayer(clusterGroup);
         }
 			}
 	);
-
 });
